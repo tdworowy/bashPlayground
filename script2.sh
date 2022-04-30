6f91ec36a624bae05030e5ba61dea3f56a8613cf
Tomasz Dworowy <dworowytomasz@gmail.com>
2022-04-30 11:10:04 +0200

    basic examples

---
+++ script2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+var="8dm7KsjU28B7v621Jls"
+value="ERmFRMVZ0U2paTlJYTkxDZz09Cg"
+
+for i in {1..40}
+do
+        var=$(echo $var | base64)
+		
+	if [[ "$var" == *"$value"* && ${var#} > 113469 ]] 
+
+        then
+	    
+	    echo ${#var}
+	    echo $var | tail -c 20	
+            echo ${var: -19}  
+	 
+       fi
+done
+
